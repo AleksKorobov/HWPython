f6b70cc71160cb5aa7b88e8d9017258c104d2dcf
Aleks_K <korobov_a_u@mail.ru>
2022-12-15 13:18:42 +0500

    go

--- HW/121222_1/2.py
+++ HW/121222_1/2.py
@@ -1,2 +1,9 @@
 # Напишите программу для. проверки истинности утверждения
 #  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+print("Введите значение x:")
+x = int(input())
+print("Введите значение у:")
+y = int(input())
+print("Введите значение z:")
+z = int(input())
+print(not(x or y or z)==(not x and not y and not z))
